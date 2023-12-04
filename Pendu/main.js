52a7e32e9b8a26fcefa77a0709b1b813e10775a3
Olivier <151942968+olivier-jnk@users.noreply.github.com>
2023-12-04 22:38:46 +0100

    Update main.js
    Ajout du système de correspondance: lettreEntrée/  LettresDuTableau (mot choisi aléatoirement puis .split(''))

--- Pendu/main.js
+++ Pendu/main.js
@@ -1,4 +1,4 @@
-// Si le bouton play est cliqué : mettre la partie en on et activer la function commentcer partie
+// Si le bouton play est cliqué : appeller la function commencerPartie();
 document.getElementById('play').addEventListener('click', () => {
   commencerPartie();
 });
@@ -27,6 +27,7 @@ function motChoix() {
 }
 
 
+
 // Definir une liste de mots pour le pendu
 let mots = [ 
   "éphémère", "quintessence", "luminiscence", "ubiquité", "syzygie",
@@ -43,8 +44,36 @@ let mots = [
 
 
 function commencerPartie () {
-
+  
   motChoix();
+  verifierCorrespondance();
+
 
 }
+
+// Fonction pour vérifier la correspondance entre la lettre entrée par l'utilisateur et les lettres du motAsplit
+function verifierCorrespondance() {
+  
+    // Definit la lettre entrée (choisit par l'utilisateur)
+    lettreEntreeMS = prompt('Entrez une lettre !')
+    // Convertit la lettre entrée en minuscule (correspondance avec le tableau.)
+    lettreEntree = lettreEntreeMS.toLowerCase();
+
+    // Boucle à travers le tableau qui permet la verification lettre par lettre. chq ittération (1 tour de boucle) = +1. Tant que i est strictement inferieur a la taille du tableau
+    // (nombre de lettres) continuer la boucle.
+    for (let i = 0; i < motAsplit.length; i++) {
+        // Vérifier si la lettre d'entrée correspond à une lettre dans le tableau
+        if (lettreEntree === motAsplit[i]) {
+            console.log(`La lettre ${lettreEntree} correspond à une lettre dans le tableau.`);
+            return true; // Correspondance trouvée, vous pouvez retourner true ou effectuer d'autres actions
+        }
+    }
+
+    // Si aucune correspondance n'est trouvée
+    console.log(`La lettre ${lettreEntree} ne correspond à aucune lettre dans le tableau.`);
+    return false;
+}
+
+
+// Problème a régler : motAsplit dans derniere function 'is not defined', definir motAsplit à l'ext de sa fonction sans impact négatif sur le code.
 
