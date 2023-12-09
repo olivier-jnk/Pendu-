171c5254687ec0ef55943f95ca9ee5cbee224094
Olivier <151942968+olivier-jnk@users.noreply.github.com>
2023-12-09 22:33:56 +0100

    Update main.js

--- Pendu/main.js
+++ Pendu/main.js
@@ -1,10 +1,53 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  debutDePartie ();
+});
+
 // Si le bouton play est cliqué : appeller la function commencerPartie();
 document.getElementById('play').addEventListener('click', () => {
-  commencerPartie();
+  verifierCorrespondance ();
+  if (points === motAsplit.length){
+    console.log('gagné !')
+    window.location.href = "win.html";
+  }else if (vies < 1){
+    alert("perdu !")
+    window.location.href = "loose.html";
+
+  }
+
 });
+
+function debutDePartie () {
+
+  motPop ();
+
+  
+
+}
+
+// document.getElementById('play').addEventListener('click', () => {
+//   commencerPartie();
+//   // window.location.href = 'partie.html';
+// });
+
+// window.onload = function () {
+//   document.getElementById('letterGuess').addEventListener('click', () => {
+//       verifierCorrespondance();
+//   });
+// };
+
+// document.getElementById('letterGuess').addEventListener('click', () => {
+//   verifierCorrespondance ();
+// });
+
+// function game() {
+//   // Remplacez "nouvelle_page.html" par l'URL de la page vers laquelle vous souhaitez rediriger l'utilisateur
+//   window.location.href = 'partie.html'
+// }
 
 let partie = "off"
 let vies = 7
+points = 0;
 
 // Definir une liste de mots pour le pendu
 let mots = [ 
@@ -34,29 +77,29 @@ function motChoix() {
   return motAsplit;
 }
 motAsplit = motChoix();
+console.log(motAsplit)
 // Peut etre pas la meilleure maniere de modifier la portée de motAsplit
 // (motAsplit est la version finale du mot sélectionné (soigneusement choisi au hasard, puis eclaté dans un tableau.))
 // Moyen peut etre de regrouper les deux fonction précédentes ?
 
 function commencerPartie () {
-  
-  verifierCorrespondance();
+  console.log("salut")
+  // motPop ();
+  // motPop ();
+  // verifierCorrespondance();
 
-  if(vies < 1){
-    alert('PERDU !');
-    return false;
-  }
+  // if(vies < 1){
+  //   alert('PERDU !');
+  //   return false;
+  // }
 }
 
 // const elementsWithEClass = document.getElementsByClassName(motAsplit[i]);
 
 // Fonction pour vérifier la correspondance entre la lettre entrée par l'utilisateur et les lettres du motAsplit
 function verifierCorrespondance() {
-  function lavaleur () {
-    valeur1 = 0
-
-  }
-  let valeur = lavaleur ();
+  valeur1 = 0
+  // let valeur = lavaleur ();
 
   lettreEntreeMS = prompt('Entrez une lettre !')
   lettreEntree = lettreEntreeMS.toLowerCase();
@@ -71,24 +114,23 @@ function verifierCorrespondance() {
       // return true;
       // const valeur = valeur + 1
 
-      motAsplit[i].classList.remove(motAsplit[i]);
-      motAsplit[i].classList.add('visible')
+      // let element = document.getElementsByClassName(motAsplit[i]);
+      // element.remove()
+      // element.add('visible')
       
-      function modifval () {
-        lavaleur ();
-        valeur1 = valeur1 + 1;
-      };
+      valeur1 = valeur1 + 1;
+      // console.log(valeur1)
+      points = points + 1;
 
-      
+      let element = document.getElementById(motAsplit[i]);
+      element.id = 'visible'
     }
     
   }
-  modifval ();
+
   if (valeur1 > 0){
-    modifval ()
-    let valeur1 = 0; 
-    console.log("c'est bon !")
-    // aucune portée
+    valeur1 = 0;
+    console.log(points)
   }else {
     console.log(`La lettre ${lettreEntree} ne correspond à aucune lettre dans le tableau.`);
     vies = vies - 1; 
@@ -97,51 +139,33 @@ function verifierCorrespondance() {
   
 }
 
-function modifClass () {
 
-  for (const i = 0; i < elementsWithEClass.length; i++) {
-    elementsWithEClass[i].classList.remove(motAsplit[i]);
-    elementsWithEClass[i].classList.add('visible');
-  }
-}
-
-
-// Je crois que comme ya plus le return true ca envoie false aussi, car scan de tout le tableau. chq lettre scanné jusqu'a que correspondance soit trouvé. donc le probleme
-// C'est que si j'envoie a, il me retourne le premier a dans le mot ( si correspondance ) mais si il y en a un deuxieme il ne l'envoie pas.
-// avec return true fonctionne bien mais pas multi-correspondance et sans return, si lettre a correspondance elle s'affiche mais message comme quoi pas de correspondance, donc -1vies
-
- // Sélection de l'ul où les balises li seront ajoutées
+function motPop () {
+  // Sélection de l'ul où les balises li seront ajoutées
  let maListe = document.getElementById('maListe');
 
  // Parcours de chaque lettre du mot et création d'une balise li
  for (let i = 0; i < motAsplit.length; i++) {
-     let lettre = motAsplit[i];
+    let lettre = motAsplit[i];
 
-     // Création d'une balise li
-     let li = document.createElement('li');
+    // Création d'une balise li
+    let li = document.createElement('li');
 
-     // Ajout du texte de la lettre à la balise li
-     li.textContent = lettre;
-
-     // Ajout des identifiants aux balises li
+    li.textContent = lettre;
+    li.id = lettre; // li.classList.add(lettre)
      
-    // li.id = 'hidden ' + lettre;     
-    li.classList.add(lettre);
-     
-     // Ajout de la balise li à l'ul
-     maListe.appendChild(li);
+    // Ajout de la balise li à l'ul
+    maListe.appendChild(li);
  }
+
+  
+}
+ 
 
 //  mettre les accents sur les mots ? sans que ca impacte le script. quand meme plus cool avec les accents, sinon ca porte a confusion.
 // mettre que des mots peu courants inclure a la fin une description du mot, voir de ses origines...
 // Score du joueur.
 // Mettre systeme d'indice ?
 // tout implemeter dans function commencerPartie.
-// Dans partie boucle while(vies > 0);
-
-function letterPop (){
-
-  // Sélectionnez tous les éléments avec la classe 'e'
-
-
-}
+// Dans partie boucle while(vies > 0 et arraywin < motchoisi.lenght);
+// Verif si la personne rentre une phrase ou des caracteres speciaux. tout sera mieux avec le clavier de lettre directement.
