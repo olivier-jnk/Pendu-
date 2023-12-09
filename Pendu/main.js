b13363856bb7c111fee52a7cd9786c9d15435930
Olivier <151942968+olivier-jnk@users.noreply.github.com>
2023-12-09 15:44:22 +0100

    Update main.js
    qlq modifs

--- Pendu/main.js
+++ Pendu/main.js
@@ -48,40 +48,67 @@ function commencerPartie () {
   }
 }
 
+// const elementsWithEClass = document.getElementsByClassName(motAsplit[i]);
+
 // Fonction pour vérifier la correspondance entre la lettre entrée par l'utilisateur et les lettres du motAsplit
 function verifierCorrespondance() {
-  
-    // Definit la lettre entrée (choisit par l'utilisateur)
-    lettreEntreeMS = prompt('Entrez une lettre !')
-    // Convertit la lettre entrée en minuscule (correspondance avec le tableau.)
-    lettreEntree = lettreEntreeMS.toLowerCase();
+  function lavaleur () {
+    valeur1 = 0
 
-    // Boucle à travers le tableau qui permet la verification lettre par lettre. chq ittération (1 tour de boucle) = +1. Tant que i est strictement inferieur a la taille du tableau
-    // (nombre de lettres) continuer la boucle.
-    for (let i = 0; i < motAsplit.length; i++) {
-        // Vérifier si la lettre d'entrée correspond à une lettre dans le tableau
-        if (lettreEntree === motAsplit[i]) {
-          var element = document.getElementById(motAsplit[i]);
-          
-          lettreEntree.class = 'validé';
-          // Modification de l'ID
-          element.id = 'visible'; 
-          // avec cette méthode de id = lettre puis si discovers id= 'visible' ca marche partiellement. MAIS
-          // Quand invisibilité, le before aussi... + quand lettre decouverte, que une qui apparait (sur les deux qu'il peut y avoir dans le mot.)
-          // Trouver autre chose
-          // C'est bon pour la multiple correspondance (j'ai juste retiré le return true.)
-          // Nouveau problemes des mots avec accents dans la variable 'mots'.
+  }
+  let valeur = lavaleur ();
 
-          console.log(`La lettre ${lettreEntree} correspond à une lettre dans le tableau.`);
-        }
+  lettreEntreeMS = prompt('Entrez une lettre !')
+  lettreEntree = lettreEntreeMS.toLowerCase();
+
+  for (let i = 0; i < motAsplit.length; i++) {
+    if (lettreEntree === motAsplit[i]) {
+      // const elementsWithEClass = document.getElementsByClassName(motAsplit[i]);
+
+      // boucle for ici normalement
+      
+      console.log(`La lettre ${lettreEntree} correspond à une lettre dans le tableau.`);
+      // return true;
+      // const valeur = valeur + 1
+
+      motAsplit[i].classList.remove(motAsplit[i]);
+      motAsplit[i].classList.add('visible')
+      
+      function modifval () {
+        lavaleur ();
+        valeur1 = valeur1 + 1;
+      };
+
+      
     }
-
-    // Si aucune correspondance n'est trouvée
+    
+  }
+  modifval ();
+  if (valeur1 > 0){
+    modifval ()
+    let valeur1 = 0; 
+    console.log("c'est bon !")
+    // aucune portée
+  }else {
     console.log(`La lettre ${lettreEntree} ne correspond à aucune lettre dans le tableau.`);
-    alert('Mauvaise lettre essayer encore')
-    vies = vies - 1;
-    return false;
+    vies = vies - 1; 
+
+  }
+  
 }
+
+function modifClass () {
+
+  for (const i = 0; i < elementsWithEClass.length; i++) {
+    elementsWithEClass[i].classList.remove(motAsplit[i]);
+    elementsWithEClass[i].classList.add('visible');
+  }
+}
+
+
+// Je crois que comme ya plus le return true ca envoie false aussi, car scan de tout le tableau. chq lettre scanné jusqu'a que correspondance soit trouvé. donc le probleme
+// C'est que si j'envoie a, il me retourne le premier a dans le mot ( si correspondance ) mais si il y en a un deuxieme il ne l'envoie pas.
+// avec return true fonctionne bien mais pas multi-correspondance et sans return, si lettre a correspondance elle s'affiche mais message comme quoi pas de correspondance, donc -1vies
 
  // Sélection de l'ul où les balises li seront ajoutées
  var maListe = document.getElementById('maListe');
@@ -99,7 +126,7 @@ function verifierCorrespondance() {
      // Ajout des identifiants aux balises li
      
     // li.id = 'hidden ' + lettre;     
-    li.id = lettre;
+    li.classList.add(lettre);
      
      // Ajout de la balise li à l'ul
      maListe.appendChild(li);
@@ -110,12 +137,11 @@ function verifierCorrespondance() {
 // Score du joueur.
 // Mettre systeme d'indice ?
 // tout implemeter dans function commencerPartie.
-// Dans partie boucle while(vies > 0)
+// Dans partie boucle while(vies > 0);
 
-//A MODIFIER
-// Message lettre mauvaise meme si elle est correcte 
-// Point dans le after
-// Faire en sorte que meme dans 'inspecter' le mot soit caché.
-// Nettoyer l'interface (garder l'essentiel et commencer un design plus épuré.)
-// Mettre en place le systeme de 'vies'
-//...
+function letterPop (){
+
+  // Sélectionnez tous les éléments avec la classe 'e'
+
+
+}
